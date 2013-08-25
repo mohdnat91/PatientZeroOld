24460da603f21c05a41aaa59ce34ee2d09b7920c
neo <neothedm@gmail.com>
2013-08-25 10:29:04 -0700

    test

---
+++ PatientZero/Controllers/SectionController.cs
@@ -0,0 +1,64 @@
+﻿using PatientZero.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PatientZero.Controllers
+{
+    public class SectionController : Controller
+    {
+
+        public ActionResult Create(string type)
+        {
+            Section section = EntityManager.Instance.InstantiateSection(type);
+            return PartialView("Details", section);
+        }
+
+        //
+        // GET: /Section/Save/SectionId
+        [HttpPost]
+        public ActionResult Create(Section section)
+        {
+            using (EntityContext context = new EntityContext())
+            {
+                context.Set<Section>().Add(section);
+                context.SaveChanges();
+            }
+            return Json(true);
+        }
+
+        public ActionResult Save(Section section)
+        {
+            using (EntityContext context = new EntityContext())
+            {
+                Section dbSection = context.Set<Section>().Attach(section);
+                context.Entry(section).State = System.Data.EntityState.Modified;
+                context.SaveChanges();
+            }
+            return Json(true);
+        }
+
+        public ActionResult Delete(Section section)
+        {
+            using (EntityContext context = new EntityContext())
+            {
+                context.Set<Section>().Remove(section);
+                context.SaveChanges();
+            }
+            return Json(true);
+        }
+
+        public ActionResult Details(long id)
+        {
+            Section section = null;
+            using (EntityContext context = new EntityContext())
+            {
+                section = context.Set<Section>().Find(id);
+            }
+            return PartialView(section);
+        }
+
+    }
+}
---
+++ PatientZero/Models/Binders/SectionBinder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PatientZero.Models.Binders
+{
+    public class SectionBinder : IModelBinder
+    {
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            string type = controllerContext.RequestContext.HttpContext.Request["type"];
+            Section section = EntityManager.Instance.InstantiateSection(type);
+            foreach (PropertyInfo property in section.GetType().GetProperties())
+            {
+                property.SetValue(section, controllerContext.RequestContext.HttpContext.Request[property.Name]);
+            }
+
+            return section;
+        }
+    }
+}
